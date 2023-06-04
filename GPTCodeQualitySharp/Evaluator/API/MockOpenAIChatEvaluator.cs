9b7c949f006d57d9812b1ba53870910bd1dc8354
Hunter Watson <hwvs@users.noreply.github.com>
2023-06-04 00:42:16 -0700

    edited unit tests

---
+++ GPTCodeQualitySharp/Evaluator/API/MockOpenAIChatEvaluator.cs
@@ -0,0 +1,65 @@
+﻿using GPTCodeQualitySharp.Dataset;
+using GPTCodeQualitySharp.Document.Partial;
+using GPTCodeQualitySharp.Evaluator.API.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTCodeQualitySharp.Evaluator.API
+{
+    public class MockOpenAIChatEvaluator : ICodeChunkEvaluatorAsync
+    {
+        public const string MOCK_RESULT_CONTAINS = " |mock result| "; // Something that should be in the result
+        const string MOCK_RESULT = // multiline literal
+            MOCK_RESULT_CONTAINS + @"some random stuff that needs to be cut off from the AI trying to be helpful and not following directions !*#!*@*)) ((({
+    ""summaryOfSnippet"": ""Code appears to be a method that takes a prompt and returns a result. It is difficult to determine the context of the code, but it appears to be a part of a larger application. The code is readable and follows language conventions, but there are some minor issues with the code."",
+    ""optimizationIdeas"": ""The code could be improved by adding comments to explain the purpose of the code. The method could be refactored to reduce the number of nested if statements. The method could also be refactored to reduce the number of lines of code."",
+    ""s1_pub_access"": 10,
+    ""s2_one_job"": 8,
+    ""s3_many_params"": 10,
+    ""s4_exs"": 8,
+    ""s5_disposal"": 10,
+    ""s6_patterns"": 5,
+    ""s7_readable"": 8,
+    ""s8_spaghetti"": 10,
+    ""s9_cryptic"": 10,
+    ""s10_lang"": 10,
+    ""s11_dup"": 10,
+    ""s12_complex"": 7,
+    ""s13_combexp"": 7
+} !@#*!* some random stuff that needs to be cut off from the AI trying to be helpful and not following directions";
+
+        string PromptTemplate { get; }
+
+        private readonly decimal _temperature;
+        private readonly IValueStore _valueStore;
+
+        public MockOpenAIChatEvaluator(string promptTemplate, IValueStore valueStore, string ApiKey, decimal temperature = 0)
+        {
+            PromptTemplate = promptTemplate;
+            _temperature = temperature;
+            _valueStore = valueStore;
+
+            // TODO: Make this robust
+            if (!PromptTemplate.Contains("{CODE}"))
+            {
+                throw new ArgumentException("Prompt template must contain {CODE} placeholder");
+            }
+        }
+
+        private string PreparePrompt(CodeChunk codeChunk)
+        {
+            return PromptTemplate.Replace("{CODE}", codeChunk.Code);
+        }
+
+        public async Task<EvaluatorResult> EvaluateAsync(CodeChunk codeChunk)
+        {
+            // TODO: Make this robust
+            string prompt = PreparePrompt(codeChunk);
+
+            return new EvaluatorResult(true, MOCK_RESULT);
+        }
+    }
+}
---
+++ GPTCodeQualitySharpTests/EvaluatorTests.cs
@@ -0,0 +1,108 @@
+﻿using GPTCodeQualitySharp.Dataset;
+using GPTCodeQualitySharp.Document.Partial;
+using GPTCodeQualitySharp.Document.Provider;
+using GPTCodeQualitySharp.Evaluator;
+using GPTCodeQualitySharp.Evaluator.API;
+using GPTCodeQualitySharp.Evaluator.API.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTCodeQualitySharpTests
+{
+    [TestClass]
+    public class EvaluatorTests
+    {
+        const string db_path = "tests.db"; // Cleanup! 
+
+        private static SQLiteValueStore valueStore;
+        private static MockOpenAIChatEvaluator evaluatorAPI;
+        private static DocumentEvaluator documentEvaluator;
+        private static DocumentInfo documentInfo;
+        private static string documentText;
+
+        [ClassInitialize(InheritanceBehavior.None)]
+        public static void OnClassInitialize(TestContext context)
+        {
+            // Setup all values
+            valueStore = new SQLiteValueStore(db_path);
+            evaluatorAPI = new MockOpenAIChatEvaluator("prompt goes here {CODE} {ROLE} {ROLE}\n\n", valueStore, Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? "");
+
+            documentEvaluator = new DocumentEvaluator(evaluatorAPI, valueStore);
+
+            // Setup the document
+            documentInfo = new DocumentInfo("document.txt");
+            documentText = CodeChunkReaderTests.GetExampleDocument();
+
+            // Null checks
+            Assert.IsNotNull(valueStore);
+            Assert.IsNotNull(evaluatorAPI);
+            Assert.IsNotNull(documentEvaluator);
+            Assert.IsNotNull(documentInfo);
+            Assert.IsNotNull(documentText);
+
+            Assert.IsTrue(documentText.Length > 0, "Empty document text!");
+
+        }
+
+        [TestMethod]
+        public void TestEvaluateDocument()
+        {
+            bool hadAnyResults = false;
+            // Evaluate the document
+            //var evaluatedChunks = await GetAllEvaluatedChunks();
+            // Call async from this non-async method
+            var evaluatedChunks = GetAllEvaluatedChunks().Result;
+            foreach(EvaluatedCodeChunk evaluated in evaluatedChunks)
+            {
+                hadAnyResults = true;
+                Assert.IsNotNull(evaluated, "Evaluated code chunk is null!");
+                Assert.IsNotNull(evaluated.Result, "Evaluated code chunk result is null!");
+                Assert.IsTrue(evaluated.Result.Success, "Evaluation Result.Success is false, failed");
+
+                Assert.IsTrue(evaluated.Result.JSONResult.Length > 0, "Evaluation Result.JSONResult is empty");
+                Assert.IsTrue(evaluated.Result.JSONResult.Length > 0, "Evaluation Result.JSONResult is empty");
+                Assert.IsTrue(evaluated.Result.TryGetScore(out double score), "Evaluation Result.TryGetScore failed");
+                Assert.IsTrue(score > 0, "Evaluation score is less than zero");
+
+                // Specifics in the mock
+                Assert.IsTrue(evaluated.Result.JSONResult.Contains(MockOpenAIChatEvaluator.MOCK_RESULT_CONTAINS), "Evaluation Result.JSONResult does not contain mock needle");
+            }
+
+            Assert.IsTrue(hadAnyResults, "No results from evaluation");
+        }
+
+        private async Task<List<EvaluatedCodeChunk>> GetAllEvaluatedChunks()
+        {
+            var results = new List<EvaluatedCodeChunk>();
+            await foreach (EvaluatedCodeChunk evaluated in documentEvaluator.EvaluateDocument(documentText, documentInfo,
+                new CodeChunkReaderSettings()
+                {
+                    SoftCodeChunkLengthLimit = 2048,
+                    HardCodeChunkLengthLimit = 3000
+                })) { 
+
+                results.Add(evaluated); 
+            }
+
+            return results;
+        }
+
+        // cleanup
+        [ClassCleanup]
+        public static void OnClassCleanup()
+        {
+            bool deleted = false;
+            if (File.Exists(db_path))
+            {
+                File.Delete(db_path);
+                deleted = true;
+            }
+            Assert.IsFalse(File.Exists(db_path), "DB file was not deleted - manual cleanup may be necessary!");
+            // Assert that it was deleted
+            Assert.IsTrue(deleted, "DB file was never created");
+        }
+    }
+}
