89bf9a9afe4e9ebf583db9eb72a850ff5b6b5079
Hunter Watson <hwvs@users.noreply.github.com>
2023-06-06 14:41:21 -0700

    + Added --backtrack_ratio to analyzer, --mock (Fake/Testing API)

--- GPTCodeQualitySharpAnalyzer/Program.cs
+++ GPTCodeQualitySharpAnalyzer/Program.cs
@@ -25,9 +25,11 @@ Options:
     --prompt    Path to the prompt file [default: prompt.txt]
     --apikey    OpenAI API key [default: %OPENAI_API_KEY% Environment Variable]
     --result    Path to store a JSON file with the results [default: results_###.json]
+    --backtrack_ratio  The percentage of lines to overlap between code chunk. Higher values use more API calls [default: 0.50]
 
 Experimental:
-    --temperature  Causes non-deterministic results - breaks caching and may use excessive API costs, use at own risk [default: 0] - EXPERIMENTAL!
+    --temperature      Causes non-deterministic results - breaks caching and may use excessive API costs, use at own risk [default: 0] - EXPERIMENTAL!
+    --mock_api         Use a mock API that returns random results [default: false] - EXPERIMENTAL!
 ";
             Console.WriteLine(helpdoc);
         }
@@ -53,7 +55,7 @@ Experimental:
             string dbPathFromCommandLine = GetArgument(args, "--db");
             if (dbPathFromCommandLine != "")
             {
-                dbPath = dbPathFromCommandLine;
+                dbPath = Environment.ExpandEnvironmentVariables(dbPathFromCommandLine);
             }
             else
             {
@@ -76,7 +78,7 @@ Experimental:
             }
 
             var valueStore = new SQLiteValueStore(dbPath);
-            string promptPath = GetArgument(args, "--prompt", "prompt.txt");
+            string promptPath = Environment.ExpandEnvironmentVariables(GetArgument(args, "--prompt", "prompt.txt"));
             if(!File.Exists(promptPath))
             {
                 Console.WriteLine($"Error: Prompt file \"{promptPath}\" does not exist");
@@ -98,13 +100,25 @@ Experimental:
                 Console.WriteLine("Warning: Using a non-zero temperature is experimental and may cause non-deterministic results");
             }
 
-            var evaluatorAPI = new OpenAIChatEvaluator(File.ReadAllText(promptPath), valueStore, openAIAPIKey, temperature);
+            ICodeChunkEvaluatorAsync evaluatorAPI;
+            if(false == GetArgument(args, "--mock_api", "false").Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                // Real API
+                evaluatorAPI = new OpenAIChatEvaluator(await File.ReadAllTextAsync(promptPath), valueStore, openAIAPIKey, temperature);
+            }
+            else
+            {
+                // Fake API - for testing
+                evaluatorAPI = new MockOpenAIChatEvaluator(await File.ReadAllTextAsync(promptPath), valueStore, "12345" /* Always 'valid' key */, temperature);
+                Console.WriteLine("Warning: Using mock API - results will be random");
+            }
+
             DocumentEvaluator documentEvaluator = new DocumentEvaluator(evaluatorAPI, valueStore);
 
 
 
             // Now evaluate an entire folder (recursively) based on the pattern
-            string folder = GetArgument(args, "--folder", ""); // REQUIRED
+            string folder = Environment.ExpandEnvironmentVariables(GetArgument(args, "--folder", "")); // REQUIRED
             if (folder == "")
             {
                 Console.WriteLine("Error: --folder argument is required");
@@ -121,8 +135,22 @@ Experimental:
             // All document results
             var documentResultPairsUnsorted = new List<Tuple<DocumentInfo, EvaluatedCodeChunk>>();
 
+            double backtrackRatio;
+            string backtrackRatioStr = GetArgument(args, "--backtrack_ratio", "0.5");
+            if(!double.TryParse(backtrackRatioStr, out backtrackRatio))
+            {
+                Console.WriteLine($"Error: Invalid backtrack ratio \"{backtrackRatioStr}\"");
+                return;
+            }
+            else if (backtrackRatio < 0 || backtrackRatio > 1)
+            {
+                Console.WriteLine($"Error: Backtrack ratio must be between 0 and 1");
+                return;
+            }
+
+
             // Evaluate each document
-            Regex exclusionRegex = new Regex(@"\.AssemblyAttributes\.cs|\.AssemblyInfo\.cs|\.Designer\.cs");
+            Regex exclusionRegex = new Regex(@"\.AssemblyAttributes\.cs|\.AssemblyInfo\.cs|\.Designer\.cs|\.g\.cs");
             foreach (string file in files)
             {
                 if(exclusionRegex.IsMatch(file))
@@ -132,7 +160,7 @@ Experimental:
                 }
 
                 Console.WriteLine($"Evaluating {file}...");
-                var documentResultPairs = await EvaluateDocument(documentEvaluator, folder, file);
+                var documentResultPairs = await EvaluateDocument(documentEvaluator, folder, file, backtrackRatio);
                 documentResultPairsUnsorted.AddRange(documentResultPairs);
             }
 
@@ -145,7 +173,7 @@ Experimental:
 
             // Save the results
             string timeStampInt = DateTime.Now.ToString("yyyyMMddHHmmss");
-            string resultPath = GetArgument(args, "--result", $"results_{timeStampInt}.json");
+            string resultPath = Environment.ExpandEnvironmentVariables(GetArgument(args, "--result", $"results_{timeStampInt}.json"));
 
 
             // Path
@@ -154,7 +182,10 @@ Experimental:
             // CodeChunkInfo.EndLineNumber
             var jsonObject = new JObject();
 
+            int idx = -1;
+
             foreach(var resultPair in documentResultPairsUnsorted) {
+                idx++;
                 DocumentInfo documentInfo = resultPair.Item1;
                 EvaluatedCodeChunk evaluatedCodeChunk = resultPair.Item2;
 
@@ -162,14 +193,14 @@ Experimental:
                 var resultObject = new JObject();
                 resultObject["path"] = documentInfo.RelativeFilePath;
                 double score = -1;
-                resultObject["score"] = result.TryGetScore(out score) ? score : -1;
+                resultObject["score"] = result.TryGetScore(out score) ? Math.Round(score, 4) : -1;
                 resultObject["startline"] = evaluatedCodeChunk.CodeChunkInfo.StartLineNumber;
                 resultObject["endline"] = evaluatedCodeChunk.CodeChunkInfo.EndLineNumber;
 
                 resultObject["rawResponse"] = result.JSONResult;
 
                 // Add the result to the JSON object
-                jsonObject[documentInfo.RelativeFilePath] = resultObject;
+                jsonObject[idx.ToString()] = resultObject;
             }
 
             await File.WriteAllTextAsync(resultPath, jsonObject.ToString());
@@ -197,7 +228,7 @@ Experimental:
             return defaultValue;
         }
 
-        private static async Task<List<Tuple<DocumentInfo, EvaluatedCodeChunk>>> EvaluateDocument(DocumentEvaluator documentEvaluator, string documentBase, string documentPath)
+        private static async Task<List<Tuple<DocumentInfo, EvaluatedCodeChunk>>> EvaluateDocument(DocumentEvaluator documentEvaluator, string documentBase, string documentPath, double backtrackRatio = 0.5d)
         {
             var documentResultPairsUnsorted = new List<Tuple<DocumentInfo, EvaluatedCodeChunk>>();
             string relativePath = Path.GetRelativePath(documentBase, documentPath);
@@ -208,7 +239,7 @@ Experimental:
                     {
                         SoftCodeChunkLengthLimit = 2048,
                         HardCodeChunkLengthLimit = 3000,
-                        BacktrackRatio = 0.5,
+                        BacktrackRatio = backtrackRatio,
                 }
             ))
             {
